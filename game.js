59a9f574192338d7da85eb786629233853206307
Gleb Motuz <hihoho@mail.ru>
2017-05-22 21:41:23 +0300

    isIntersectV3

--- game.js
+++ game.js
@@ -69,40 +69,43 @@ class Actor {
   }
 
   isIntersect(actor) {
-    if (actor instanceof Actor && arguments.length != 0) {
-      if (actor.left >= this.left && actor.right >= this.right && actor.top >= this.top && actor.bottom >= this.bottom) {
-        return false;
-      } else if (actor.right === this.left || actor.left === this.right || actor.top === this.bottom || actor.bottom === this.top) {
-        return false;
-      } else if (-actor.left === this.left && -actor.top === this.top && -actor.right === this.right && -actor.bottom === this.bottom) {
-        return false;
-      } else if (actor.left > this.left && actor.top > this.top && actor.right < this.right && actor.bottom < this.bottom) {
-        return true;
-      } else if (
-        (actor.left < this.left && actor.right < this.right && actor.top < this.top && actor.bottom < this.bottom) || // 1
-        (actor.left > this.left && actor.right < this.right && actor.top < this.top && actor.bottom < this.bottom) || // 2
-        (actor.left > this.left && actor.right > this.right && actor.top < this.top && actor.bottom < this.bottom) || // 3
-
-        (actor.left < this.left && actor.right < this.right && actor.top > this.top && actor.bottom < this.bottom) || // 4
-        (actor.left > this.left && actor.right > this.right && actor.top > this.top && actor.bottom < this.bottom) || // 5
-
-        (actor.left < this.left && actor.right < this.right && actor.top > this.top && actor.bottom > this.bottom) || // 6
-        (actor.left > this.left && actor.right < this.right && actor.top > this.top && actor.bottom > this.bottom) || // 7
-        (actor.left > this.left && actor.right > this.right && actor.top > this.top && actor.bottom > this.bottom) || // 8
-
-        (actor.left < this.left && actor.right > this.right && actor.top < this.top && actor.bottom < this.bottom) || // 9
-        (actor.left < this.left && actor.right > this.right && actor.top > this.top && actor.bottom < this.bottom) || // 10
-        (actor.left < this.left && actor.right > this.right && actor.top > this.top && actor.bottom > this.bottom) || // 11
-
-        (actor.left < this.left && actor.right < this.right && actor.top < this.top && actor.bottom > this.bottom) || // 12
-        (actor.left > this.left && actor.right < this.right && actor.top < this.top && actor.bottom > this.bottom) || // 13
-        (actor.left > this.left && actor.right > this.right && actor.top < this.top && actor.bottom > this.bottom)    // 14
-      ) {
-        return true;
-      }
-    } else {
+    if (!(actor instanceof Actor)) {
       throw new Error('actor не является Actor')
     }
+
+    if (actor.left === this.left && actor.right === this.right && actor.top === this.top && actor.bottom === this.bottom) {
+      return false;
+    } else if (actor.right < this.left || actor.bottom < this.top && actor.left < this.right || actor.top > this.bottom) {
+      return false;
+    } else if (actor.right === this.left || actor.left === this.right || actor.top === this.bottom || actor.bottom === this.top) {
+      return false;
+    } else if (-actor.left === this.left && -actor.top === this.top && -actor.right === this.right && -actor.bottom === this.bottom) {
+      return false;
+    } else if (actor.left > this.left && actor.top > this.top && actor.right < this.right && actor.bottom < this.bottom) {
+      return true;
+    } else if (
+      (actor.left < this.left && actor.right < this.right && actor.top < this.top && actor.bottom < this.bottom) || // 1
+      (actor.left > this.left && actor.right < this.right && actor.top < this.top && actor.bottom < this.bottom) || // 2
+      (actor.left > this.left && actor.right > this.right && actor.top < this.top && actor.bottom < this.bottom) || // 3
+
+      (actor.left < this.left && actor.right < this.right && actor.top > this.top && actor.bottom < this.bottom) || // 4
+      (actor.left > this.left && actor.right > this.right && actor.top > this.top && actor.bottom < this.bottom) || // 5
+
+      (actor.left < this.left && actor.right < this.right && actor.top > this.top && actor.bottom > this.bottom) || // 6
+      (actor.left > this.left && actor.right < this.right && actor.top > this.top && actor.bottom > this.bottom) || // 7
+      (actor.left > this.left && actor.right > this.right && actor.top > this.top && actor.bottom > this.bottom) || // 8
+
+      (actor.left < this.left && actor.right > this.right && actor.top < this.top && actor.bottom < this.bottom) || // 9
+      (actor.left < this.left && actor.right > this.right && actor.top > this.top && actor.bottom < this.bottom) || // 10
+      (actor.left < this.left && actor.right > this.right && actor.top > this.top && actor.bottom > this.bottom) || // 11
+
+      (actor.left < this.left && actor.right < this.right && actor.top < this.top && actor.bottom > this.bottom) || // 12
+      (actor.left > this.left && actor.right < this.right && actor.top < this.top && actor.bottom > this.bottom) || // 13
+      (actor.left > this.left && actor.right > this.right && actor.top < this.top && actor.bottom > this.bottom)    // 14
+    ) {
+      return true;
+    }
+
   }
 }
 
