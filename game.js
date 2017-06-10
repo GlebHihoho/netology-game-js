9325446be45eb91039e4a4c19d78f3e8ab7a85be
Gleb Motuz <hihoho@mail.ru>
2017-06-10 13:27:18 +0200

    final2.5

--- game.js
+++ game.js
@@ -14,7 +14,7 @@ class Vector {
     return new Vector(this.x + vector.x, this.y + vector.y);
   }
 
-times(n){
+  times(n){
     return new Vector(this.x * n, this.y * n);
   }
 }
@@ -22,7 +22,13 @@ times(n){
 
 class Actor {
   constructor(pos = new Vector(0, 0), size = new Vector(1, 1), speed = new Vector(0, 0)) {
-    if (!(pos instanceof Vector && size instanceof Vector && speed instanceof Vector)) {
+    if (
+        !(
+          pos   instanceof Vector &&
+          size  instanceof Vector &&
+          speed instanceof Vector
+        )
+    ) {
       throw new Error('pos / size / speed не является vector')
     }
 
@@ -60,32 +66,36 @@ class Actor {
       throw new Error('actor не является Actor')
     };
 
+    // если разбить на не несколько if будет понятнее
+
+    // Глеб: не совсем понятно как разбить этот if на несколько, ведь они должны срабарывать вместе
+
     if (
-      actor.left < this.right &&
-      actor.right > this.left &&
-      actor.top < this.bottom &&
-      actor.bottom > this.top &&
+      actor.left    < this.right &&
+      actor.right   > this.left &&
+      actor.top     < this.bottom &&
+      actor.bottom  > this.top &&
       actor !== this
     ) {
       return true;
-    } else {
-      return false;
     }
+
+    return false;
   }
 }
 
 class Level {
   constructor(grid = [], actors = []) {
-    this.grid = grid.slice();
+    this.grid   = grid.slice();
     this.actors = actors.slice();
     this.height = this.grid.length;
     this.status = null;
     this.finishDelay = 1;
 
     if (this.height !== 0) {
-      let arrLength = this.grid.map(element => element.length);
+      let lengthArr = this.grid.map(element => element.length);
 
-      this.width = Math.max.apply(null, arrLength);
+      this.width = Math.max(...lengthArr);
     } else {
       this.width = 0;
     }
@@ -110,31 +120,34 @@ class Level {
     let yEnd   = Math.ceil(pos.y + size.y);
 
     if (xStart < 0 || xEnd > this.width || yStart < 0) {
-      return "wall";
+      return 'wall';
     }
 
     if (yEnd > this.height) {
-      return "lava";
+      return 'lava';
     }
 
     for (let y = yStart; y < yEnd; y++) {
       for (let x = xStart; x < xEnd; x++) {
         let fieldType = this.grid[y][x];
-        if (fieldType) return fieldType;
+
+        if (fieldType) {
+          return fieldType;
+        }
       }
     }
   }
 
   removeActor(actor) {
-    let index = 0;
+    let findIndex = 0;
 
     for (let i = 0; i < this.actors.length; i++) {
       if (this.actors[i] === actor) {
-        index = i;
+        findIndex = i;
       }
     }
 
-    this.actors.splice(index, 1);
+    this.actors.splice(findIndex, 1);
   }
 
   noMoreActors(type) {
@@ -156,12 +169,67 @@ class Level {
   }
 }
 
-class Player extends Actor {
-  constructor(pos = new Vector(0, 0), size = new Vector(0.8, 1.5)) {
-    super(pos, size)
+class LevelParser {
+  constructor(symbol) {
+    this.symbol = symbol;
+  }
 
-    this.pos = pos.plus(new Vector(0, -0.5));
-    this.size = size;
+  actorFromSymbol(str) {
+    if (this.symbol) {
+      return this.symbol[str];
+    }
+  }
+
+  obstacleFromSymbol(str) {
+    switch(str) {
+      case 'x' : return 'wall';
+      case '!' : return 'lava';
+      default  : return undefined;
+    }
+  }
+
+  createGrid(plan) {
+    if (!plan.length) {
+      return [];
+    }
+
+    return plan.map(line => {
+      return line.split('').map(ch => this.obstacleFromSymbol(ch));
+    })
+  }
+
+  createActors(plan) {
+    let actors = [];
+
+    if (!this.symbol) {
+      return [];
+    }
+
+    for (let x = 0; x < plan.length; x++) {
+      for (let y = 0; y < plan[x].length; y++) {
+        let cell = this.symbol[plan[x][y]];
+
+        if (typeof cell === 'function') {
+          let actor = new cell(new Vector(y, x));
+          if (actor instanceof Actor) {
+            actors.push(actor);
+          }
+        }
+      }
+    }
+    return actors;
+  }
+
+  parse(plan) {
+    return new Level(this.createGrid(plan), this.createActors(plan))
+  }
+}
+
+class Player extends Actor {
+  constructor(pos) {
+    super(pos, new Vector(0.8, 1.5));
+
+    this.pos = this.pos.plus(new Vector(0, -0.5));
   }
 
   get type() {
@@ -180,6 +248,8 @@ class Fireball extends Actor {
 
   getNextPosition(time = 1) {
     // лучше обратить условие, чтобы уменьшить вложенность
+
+    // Глеб: что-то я туплю в этом месте)
     if (this.speed) {
       return this.pos.plus(this.speed.times(time));
     }
@@ -192,7 +262,17 @@ class Fireball extends Actor {
 
   act(time, level) {
     let newPos = this.getNextPosition(time);
+
     if (level.obstacleAt(newPos, this.size)) {
+      // лучше не писать return с функцией которая ничего не возвращает
+      // может показаться, что там есть какой-то результат
+      // лучше 2 строчки - вызов функции и return;
+
+      // Gleb : я понял что вы хотите сделать. но из-за этих изменений тесты выдают ошибку
+      // if (level.obstacleAt(newPos, this.size)) {
+      //  this.handleObstacle();
+      // }
+      // return this.pos = newPos;
       return this.handleObstacle();
     }
 
@@ -238,14 +318,15 @@ function random(min, max) {
   return rand;
 }
 
-const phaseStart = 0;
+const phaseStart  = 0;
 const phaseFinish = 2 * Math.PI;
 
 class Coin extends Actor {
   constructor(pos) {
     super(pos, new Vector(0.6, 0.6));
-
+    // должно задаваться через конструктор базового класса
     this.pos  = this.pos.plus(new Vector(0.2, 0.1));
+    this.post = this.pos;
     this.spring = random(phaseStart, phaseFinish);
     this.springDist = 0.07;
     this.springSpeed = 8;
@@ -266,7 +347,7 @@ class Coin extends Actor {
 
   getNextPosition(time = 1) {
     this.updateSpring(time);
-    return this.pos.plus(this.getSpringVector());
+    return this.post.plus(this.getSpringVector());
   }
 
   act(time) {
@@ -274,75 +355,108 @@ class Coin extends Actor {
   }
 }
 
-class LevelParser {
-  constructor(symbol) {
-    this.symbol = symbol;
-  }
-
-  actorFromSymbol(str) {
-    if (this.symbol) {
-      return this.symbol[str];
-    }
-  }
-
-  obstacleFromSymbol(str) {
-    switch(str) {
-      case 'x' : return 'wall';
-      case '!' : return 'lava';
-      default  : return undefined;
-    }
-  }
-
-  createGrid(arrString) {
-    return arrString.map(char => {
-      return char.split('').map(ch => this.obstacleFromSymbol(ch));
-    })
-  }
-
-  createActors(plan) {
-    let actors = [];
-    if (this.symbol === undefined) return [];
-
-    for (let x = 0; x < plan.length; x++) {
-      for (let y = 0; y < plan[x].length; y++) {
-
-        if (typeof this.symbol[plan[x][y]] === 'function') {
-          let actor = new this.symbol[plan[x][y]](new Vector(y, x));
-          if (actor instanceof Actor) {
-            actors.push(actor);
-          }
-        }
-      }
-    }
-    return actors;
-  }
-
-  parse(plan) {
-    return new Level(this.createGrid(plan), this.createActors(plan))
-  }
-}
-
+// если собрать монетку после столкновения с фаерболом, то уровень всё расно считается выигранным
+// Gleb : ну это фича такая)
 
 const schemas = [
   [
-    "     v                 ",
     "                       ",
     "                       ",
     "                       ",
     "                       ",
-    "  |xxx       w         ",
+    "                       ",
+    "   xxx     x w         ",
     "  o                 o  ",
-    "  x               = x  ",
+    "  x             x = x  ",
     "  x          o o    x  ",
     "  x  @    *  xxxxx  x  ",
     "  xxxxx             x  ",
     "      x!!!!!!!!!!!!!x  ",
     "      xxxxxxxxxxxxxxx  ",
     "                       "
+  ],
+  [
+    "     v                 ",
+    "                       ",
+    "                       ",
+    "                       ",
+    "                       ",
+    "  |                    ",
+    "  o                 o  ",
+    "  x               = x  ",
+    "  x          o o    x  ",
+    "  x  @       xxxxx  x  ",
+    "  xxxxx             x  ",
+    "      x!!!!!!!!!!!!!x  ",
+    "      xxxxxxxxxxxxxxx  ",
+    "                       "
+  ],
+  [
+    "        |           |  ",
+    "                       ",
+    "                       ",
+    "                       ",
+    "                       ",
+    "                       ",
+    "                       ",
+    "                       ",
+    "                       ",
+    "     |                 ",
+    "                       ",
+    "         =      |      ",
+    " @ |  o            o   ",
+    "xxxxxxxxx!!!!!!!xxxxxxx",
+    "                       "
+  ],
+  [
+    "                       ",
+    "                       ",
+    "                       ",
+    "    o                  ",
+    "    x      | x!!x=     ",
+    "         x             ",
+    "                      x",
+    "                       ",
+    "                       ",
+    "                       ",
+    "               xxx     ",
+    "                       ",
+    "                       ",
+    "       xxx  |          ",
+    "                       ",
+    " @                     ",
+    "xxx                    ",
+    "                       "
+  ],
+  [
+    "   v         v",
+    "              ",
+    "         !o!  ",
+    "              ",
+    "              ",
+    "              ",
+    "              ",
+    "         xxx  ",
+    "          o   ",
+    "        =     ",
+    "  @           ",
+    "  xxxx        ",
+    "  |           ",
+    "      xxx    x",
+    "              ",
+    "          !   ",
+    "              ",
+    "              ",
+    " o       x    ",
+    " x      x     ",
+    "       x      ",
+    "      x       ",
+    "   xx         ",
+    "              "
   ]
 ];
 
-var actorDict = {
+const actorDict = {
   "@": Player,
   "=": HorizontalFireball,
   "|": VerticalFireball,
@@ -352,4 +466,4 @@ var actorDict = {
 
 const parser = new LevelParser(actorDict);
 runGame(schemas, parser, DOMDisplay)
-  .then(() => console.log('Вы выиграли приз!'));
+  .then(() => alert('Вы выиграли приз!'));
